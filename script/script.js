6392b3a49434da50347f9c378e6727ab14ff865d
Fernando Maciel <fernandonevesmaciel@gmail.com>
2025-05-30 21:06:45 -0300

    criando pagina tutoriais

--- script/script.js
+++ script/script.js
@@ -1,8 +1,23 @@
 const botao = document.querySelector('.botao-menu')
 const menuLateral = document.querySelector('.menu-lateral')
 const background = document.querySelector('.background')
+const menu = document.querySelector('#menu')
 
 botao.addEventListener('click', () =>{
     menuLateral.classList.toggle('ativo')
     botao.classList.toggle('ativo')
-})
+
+    if (menu.classList.contains('fa-bars')) {
+        menu.classList.remove('fa-bars');
+        menu.classList.add('fa-right-from-bracket');
+    } else {
+        menu.classList.remove('fa-right-from-bracket');
+        menu.classList.add('fa-bars');
+    }
+}
+)
+
+
+
+
+
