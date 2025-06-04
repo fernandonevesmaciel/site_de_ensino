ddd3ef95626bccfd192ab911002aebc8576e5be5
Fernando Maciel <fernandonevesmaciel@gmail.com>
2025-06-03 23:45:50 -0300

    adicionando conteudo no tutorial do inversor cfw500

--- tutoriais/script/script.js
+++ tutoriais/script/script.js
@@ -50,6 +50,109 @@ function carregar(){
         `
     )
 
+    criarLinha(
+        "Programações do inversor",
+        `
+            <h2> Programando gira/para com chave seccionadora</h2> <br><br>
+            
+            <img src="../imagens/gira-para chave seletora.jpg" alt="inversor com chave seletora em modo gira/para" class="imagemTutorial"> <br>
+
+            <p id="textoTutorial">
+                Com a chave seletora ligada nos bornes 1 = dl1 e no borne 9 = 24v do inversor, estamos a usar o inversor em PNP.
+                essa ligação geralmente usada para esteiras ou funcionalidades onde o inversor tem o objetivo de rodar somente quando a chave seletora estiver acionada ou seja caso desacione o inversor irá parar conforme a rampa programada.<br><br>
+
+                Parâmetros de programação para GIRA/PARA:<br><br>
+
+                   <table>
+            <thead>
+                <tr>
+                    <th>Parâmetros</th>
+                    <th>Valor usado</th>
+                    <th>Descrição</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>P000</td>
+                    <td>5</td>
+                    <td>Senha de acesso para modificar parâmetros</td>
+                </tr>
+                <tr>
+                    <td>P204</td>
+                    <td>5</td>
+                    <td>Reseta o inversor de Fábrica</td>
+                </tr>
+                <tr>
+                    <td>P100</td>
+                    <td>1.0</td>
+                    <td>Rampa de aceleração</td>
+                </tr>
+                <tr>
+                    <td>P101</td>
+                    <td>0.5</td>
+                    <td>Rampa de desaceleração</td>
+                </tr>
+                 <tr>
+                    <td>P133</td>
+                    <td>0.0</td>
+                    <td>Frequência minima</td>
+                </tr>
+                 <tr>
+                    <td>P134</td>
+                    <td>60.0</td>
+                    <td>Frequência máxima</td>
+                </tr>
+                <tr>
+                    <td>P156</td>
+                    <td>1.87</td>
+                    <td>Corrente 1.1x a nominal do motor</td>
+                    
+                </tr>
+                 <tr>
+                    <td>P157</td>
+                    <td>1.75</td>
+                    <td>Corrente 1x a nominal do motor</td>
+                </tr>
+                 <tr>
+                    <td>P158</td>
+                    <td>1.46</td>
+                    <td>Corrente 0.8x a nominal do motor</td>
+                </tr>
+                 
+                 <tr>
+                    <td>P220</td>
+                    <td> 1 </td>
+                    <td>Seleciona acesso Remoto ou Local</td>
+                </tr>
+                 <tr>
+                    <td>P222</td>
+                    <td> 0 </td>
+                    <td>Referência de velocidade Remoto</td>
+                </tr>
+                 <tr>
+                    <td>P226</td>
+                    <td> 1 </td>
+                    <td>Sentido de giro Remoto</td>
+                </tr>
+                 <tr>
+                    <td>P271</td>
+                    <td> 8 </td>
+                    <td>Sinal das entradas digitais</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <br><br>
+
+        Comentario de cada parâmetro e o porque foi usado cada valor nele:<br>
+        P000 = conforme explicado na própria tabela esse parâmetro quando já programado, serve para proteger a alteração dos parâmetros só podendo ter acesso aos parâmetros quando digitado corretamente a senha.(essa senha pode ser alterada ou adicionada no parâmetro P200.)<br>
+        P204 = 
+
+
+            </p>
+        `
+    )
+
 }
 
 carregar();
